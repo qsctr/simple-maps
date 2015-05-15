a5934fba7a95758a3194f49238a58b501d0d298c
qsctr <brettonchen@gmail.com>
2015-05-15 22:24:14 +0800

    Incomplete theme color picker

---
+++ bower_components/material-card/README.md
@@ -0,0 +1,20 @@
+#material-card
+A custom element for simple cards. Built with Polymer, inspired by Material Design.
+
+![Styled example](https://github.com/lemabu/material-card/blob/master/screenshot.png  "Styled example")
+![Real world example of material-card in a project](https://github.com/lemabu/material-card/blob/master/screenshot2.png  "Real world example of material-card in a project")
+
+##Attributes
+* `autoraise`
+	* *true* or *false*
+	* Default: *false*
+	* Determines if a card should raise on hover, approaching the mouse and increasing it's dropshadow
+
+##Styling
+Card content can be styled with `material-card::shadow .content`
+
+##Demo
+See the included demo.html for a quick overview
+
+
+
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("template-bound", function(){
+document.addEventListener("template-bound", function () {
   //set up variables and functions
   var template = document.querySelector("#template");
   var map = document.querySelector("#map");
@@ -7,33 +7,33 @@ document.addEventListener("template-bound", function(){
   var zoomOutButton = document.querySelector("#zoom-out");
   var initZoom = document.querySelector("#initial-zoom");
   var zoomError = document.querySelector("#init-zoom-error");
-  var checkDefaultUI = function(){
-    if(template.defaultUI){
+  var checkDefaultUI = function () {
+    if (template.defaultUI) {
       map.disableDefaultUI = false;
     }
-    else{
+    else {
       map.disableDefaultUI = true;
     }
   };
-  var ZBDisplayOn = function(){
+  var ZBDisplayOn = function () {
     zoomInButton.style.display = "initial";
     zoomOutButton.style.display = "initial";
   };
-  var ZBDisplayOff = function(){
+  var ZBDisplayOff = function () {
     zoomInButton.style.display = "none";
     zoomOutButton.style.display = "none";
   };
-  var checkZoomButtonsColor = function(){
-    if(template.zoomLevel === 20){
+  var checkZoomButtonsColor = function () {
+    if (template.zoomLevel === 20) {
       zoomInButton.style.color = "rgba(0, 0, 0, 0.26)";
     }
-    else{
+    else {
       zoomInButton.style.color = "white";
     }
-    if(template.zoomLevel === 0){
+    if (template.zoomLevel === 0) {
       zoomOutButton.style.color = "rgba(0, 0, 0, 0.26)";
     }
-    else{
+    else {
       zoomOutButton.style.color = "white";
     }
   };
@@ -42,35 +42,35 @@ document.addEventListener("template-bound", function(){
   checkZoomButtonsColor();
 
   //add event listeners
-  document.querySelector("#defaultUI").addEventListener("core-localstorage-load", function(){
+  document.querySelector("#defaultUI").addEventListener("core-localstorage-load", function () {
     checkDefaultUI();
   });
-  document.querySelector("#showZoomButtons").addEventListener("core-localstorage-load", function(){
-    if(document.querySelector("#showZoomButtons").value === true || 
+  document.querySelector("#showZoomButtons").addEventListener("core-localstorage-load", function () {
+    if (document.querySelector("#showZoomButtons").value === true ||
       document.querySelector("#showZoomButtons").value === null ||
-      localStorage.getItem("showZoomButtons") === null){
+      localStorage.getItem("showZoomButtons") === null) {
       document.querySelector("#zoom-buttons-checkbox").checked = true;
       ZBDisplayOn();
     }
-    else{
+    else {
       document.querySelector("#zoom-buttons-checkbox").checked = false;
       ZBDisplayOff();
     }
   });
-  document.querySelector("#initialZoom").addEventListener("core-localstorage-load", function(){
-    if(template.initialZoom === null){
+  document.querySelector("#initialZoom").addEventListener("core-localstorage-load", function () {
+    if (template.initialZoom === null) {
       template.initialZoom = 10;
       console.log("zoom is null");
       console.log("zoom is " + template.initialZoom);
     }
-    else if(template.initialZoom <= 20 && template.initialZoom >= 0){
+    else if (template.initialZoom <= 20 && template.initialZoom >= 0) {
       console.log(template.initialZoom);
       console.log(template.initialZoom !== null);
       console.log(template.initialZoom <= 20);
       console.log(template.initialZoom >= 0);
       console.log("zoom valid");
     }
-    else{
+    else {
       template.initialZoom = 10;
       console.log("zoom invalid");
       console.log("zoom is " + template.initalZoom);
@@ -78,43 +78,48 @@ document.addEventListener("template-bound", function(){
     template.zoomLevel = template.initialZoom;
     console.log("zoom is now " + template.zoomLevel);
   });
-  document.querySelector("#settings-menu").addEventListener("change", function(){
+  document.querySelector("#settings-menu").addEventListener("change", function () {
     checkDefaultUI();
-    if(document.querySelector("#zoom-buttons-checkbox").checked === true){
+    if (document.querySelector("#zoom-buttons-checkbox").checked === true) {
       ZBDisplayOn();
     }
-    else{
+    else {
       ZBDisplayOff();
     }
-    document.querySelector("#showZoomButtons").value = 
+    document.querySelector("#showZoomButtons").value =
     document.querySelector("#zoom-buttons-checkbox").checked;
   });
-  slider.addEventListener("core-change", function(){
+  document.querySelector("#theme-menu-ripple").addEventListener("core-transitionend", function () {
+    if (document.querySelector("#theme-picker") !== null) {
+      document.querySelector("#theme-picker").open();
+    }
+  });
+  slider.addEventListener("core-change", function () {
     checkZoomButtonsColor();
   });
-  zoomInButton.addEventListener("click", function(){
+  zoomInButton.addEventListener("click", function () {
     slider.increment();
   });
-  zoomOutButton.addEventListener("click", function(){
+  zoomOutButton.addEventListener("click", function () {
     slider.decrement();
   });
-  initZoom.addEventListener("input", function(){
+  initZoom.addEventListener("input", function () {
     document.querySelector("#init-zoom-decor").isInvalid = !initZoom.validity.valid;
-    if(initZoom.validity.valid){
+    if (initZoom.validity.valid) {
       zoomError.opened = false;
     }
-    else{
+    else {
       zoomError.opened = true;
     }
   });
-  document.querySelector("#about-icon").addEventListener("click", function(){
-    if(document.querySelector("#about-dialog") !== null){
+  document.querySelector("#about-icon").addEventListener("click", function () {
+    if (document.querySelector("#about-dialog") !== null) {
       document.querySelector("#about-dialog").open();
     }
   });
 
   //for testing
-  document.querySelector("#ls-clear").addEventListener("click", function(){
+  document.querySelector("#ls-clear").addEventListener("click", function () {
     localStorage.clear();
   });
 });
